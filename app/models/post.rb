d3fa32dc3957068d66705509e01f503ca798e9c7
Everett <everett.bowline@gmail.com>
2018-10-11 21:22:18 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -9,11 +9,11 @@ class Post < ActiveRecord::Base
   end
 
   def self.from_today
-
+    @posts = Post.where("created_at >=?", Time.zone.today.beginning_of_day)
   end
 
   def self.old_news
-
+    @posts = Post.where("created_at <?", Time.zone.today.beginning_of_day)
   end
 
   private
